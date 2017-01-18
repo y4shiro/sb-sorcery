72514690686d509429cfd3350cdeb4c61386b968
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-01-18 12:31:55 +0900

    Modified user MVC

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
-class User < ApplicationRecord
+class User < ApplicationRecord::Base
+  authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
+
 end
