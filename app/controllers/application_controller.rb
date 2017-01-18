57cf4230d4ca31017de46bdf6eedec50e0491c2c
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-01-18 15:02:23 +0900

    Restrict the operation of not logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_filter :require_login
+
   protect_from_forgery with: :exception
+
+  private
+
+    def not_authenticated
+      redirect_to login_path, alert: "Please login first"
+    end
 end
