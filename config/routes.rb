0026faca670c49464720199fd41e042db7f2a417
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-01-18 12:57:14 +0900

    Generated Usersession Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
   resources :users
+
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
