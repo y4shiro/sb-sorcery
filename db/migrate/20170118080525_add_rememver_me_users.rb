7b31454302c739f43effdd5f467dacd19026c499
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-01-18 17:08:14 +0900

    Add remember-me to user table

---
+++ db/migrate/20170118080525_add_rememver_me_users.rb
@@ -0,0 +1,18 @@
+class AddRememverMeUsers < ActiveRecord::Migration[5.0]
+  def change
+  end
+
+  def self.up
+    add_column :users, :remember_me_token, :string, :default => nil
+    add_column :users, :remember_me_token_expires_at, :datetime, :default => nil
+
+    add_index :users, :remember_me_token
+  end
+
+  def self.down
+    remove_index :users, :remember_me_token
+
+    remove_column :users, :remember_me_token_expires_at
+    remove_column :users, :remember_me_token
+  end
+end
