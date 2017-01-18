de4f9d6eefa55273705c686a499e83709d3a377f
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-01-18 16:56:43 +0900

    Changed definition file to use username

--- db/migrate/20170118074840_sorcery_core.rb
+++ db/migrate/20170118074840_sorcery_core.rb
@@ -1,7 +1,7 @@
 class SorceryCore < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :email,            :null => false
+      t.string :username,            :null => false
       t.string :crypted_password
       t.string :salt
 
